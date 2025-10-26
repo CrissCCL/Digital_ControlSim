7dad6558be5f2202616bd8c945d60c71d21fc55b
CrissCCL <138504390+CrissCCL@users.noreply.github.com>
2025-10-26 10:03:51 -0300

    Add files via upload

--- sim_controlDigital.m
+++ sim_controlDigital.m
@@ -1,51 +1,53 @@
 clear all; close all; clc
 
-%% Discretize the plant
-Ts=0.1;
-gp=tf(20,[50 1]);
-gpd=c2d(gp,Ts,'zoh');
+%% Discretization of First-Order Identified Model (Non-Parametric)
+Ts = 0.1;
+gp  = tf(20,[50 1]);           % First-order plant
+gpd = c2d(gp,Ts,'zoh');        % Discrete plant
 
-%% Discrete PI controller parameters
-Kp=0.8;
-Ti=9;
-K0=Kp+Kp*Ts/(2*Ti);
-K1=-Kp+Kp*Ts/(2*Ti);
+%% Discrete PI Controller (Tustin)
+Kp = 0.8;
+Ti = 9;
+K0 = Kp + Kp*Ts/(2*Ti);
+K1 = -Kp + Kp*Ts/(2*Ti);
 
-%% Closed-loop simulation via direct integration
-[num,den]=tfdata(gpd,'v');
-t=0:Ts:60;
-Ref=ones(1,length(t));
-y1=0; u1=0; error1=0;
+%% Closed-Loop Simulation (Direct difference equation)
+[num,den] = tfdata(gpd,'v');
+t   = 0:Ts:60;
+Ref = ones(1,length(t));
 
-for i=1:length(t)
-    % Process simulation
-    y(i) = num(2)*u1 - den(2)*y1;
+y1 = 0; u1 = 0; e1 = 0;   % Histories
 
-    % Control computation
-    error = Ref(i) - y(i);
-    Usim(i) = u1 + K0*error + K1*error1;
+for k = 1:length(t)
+    % ---- PLANT ----
+    y(k) = num(2)*u1 - den(2)*y1;
 
-    % Saturation
-    if Usim(i) > 100
-        Usim(i) = 100;
-    elseif Usim(i) < 0
-        Usim(i) = 0;
+    % ---- FEEDBACK ----
+    e  = Ref(k) - y(k);
+    u  = u1 + K0*e + K1*e1;
+
+    % ---- SATURATION ----
+    if u > 100
+        u = 100; 
+    end
+    if u < 0
+        u = 0;   
     end
 
-    % Update states
-    y1 = y(i);
-    u1 = Usim(i);
-    error1 = error;
+    % ---- UPDATE STATES ----
+    y1 = y(k);
+    u1 = u;
+    e1 = e;
+
+    Usim(k) = u;
 end
 
-%% Plot results
+%% PLOTS
 subplot(2,1,1)
-plot(t,y,'+',t,Ref,'--','MarkerSize', 4)
-xlabel('Time [s]')
-ylabel('Response')
-legend('Sim','Ref')
+plot(t,y,'+',t,Ref,'--','MarkerSize',4)
+xlabel('Time [s]'), ylabel('Response')
+legend('Simulation','Reference')
 
 subplot(2,1,2)
-plot(t,Usim,'+','MarkerSize', 4)
-ylabel('Control output')
-xlabel('Time [s]')
+plot(t,Usim,'+','MarkerSize',4)
+xlabel('Time [s]'), ylabel('Control signal')
